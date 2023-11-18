5377540c2830164a4292fff9a856f6e75d079930
Bas Nijholt <bas@nijho.lt>
2023-11-18 11:10:47 -0800

    Correctly find pybind11

--- pybind_interface/GetPybind11.cmake
+++ pybind_interface/GetPybind11.cmake
@@ -1,15 +1,17 @@
-include(FetchContent)
-
-set(MIN_PYBIND_VERSION "2.10.1")
-FetchContent_Declare(
-  pybind11
-  GIT_REPOSITORY https://github.com/pybind/pybind11
-  GIT_TAG "v${MIN_PYBIND_VERSION}"
+# Execute python command to get pybind11 includes
+execute_process(
+    COMMAND python3 -m pybind11 --includes
+    OUTPUT_VARIABLE PYBIND11_INCLUDES
+    OUTPUT_STRIP_TRAILING_WHITESPACE
 )
-FetchContent_GetProperties(pybind11)
-find_package(pybind11 "${MIN_PYBIND_VERSION}" CONFIG)
-find_package(Python3 3.7 REQUIRED COMPONENTS Interpreter Development)
-if((NOT pybind11_FOUND) AND (NOT pybind11_POPULATED)) # check first on system path, then attempt git fetch
-  FetchContent_Populate(pybind11)
-  add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
-endif()
+
+# Split the output into a list
+string(REPLACE " " ";" PYBIND11_INCLUDE_LIST ${PYBIND11_INCLUDES})
+
+# Remove the -I prefix from each path
+list(TRANSFORM PYBIND11_INCLUDE_LIST REPLACE "^-I" "")
+
+# Include the directories
+foreach(INCLUDE_DIR ${PYBIND11_INCLUDE_LIST})
+    include_directories(${INCLUDE_DIR})
+endforeach()
