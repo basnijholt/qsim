c26a92162aa278a3ae0e8c5a25e3757395636a27
Bas Nijholt <bas@nijho.lt>
2023-11-18 11:24:31 -0800

    debug

--- pybind_interface/GetPybind11.cmake
+++ pybind_interface/GetPybind11.cmake
@@ -7,26 +7,21 @@ FetchContent_Declare(
   GIT_TAG "v${MIN_PYBIND_VERSION}"
 )
 FetchContent_GetProperties(pybind11)
+
+# Attempt to find pybind11 on the system
 find_package(pybind11 "${MIN_PYBIND_VERSION}" CONFIG)
-if((NOT pybind11_FOUND) AND (NOT pybind11_POPULATED)) # check first on system path, then attempt git fetch
+
+# Print the result of find_package
+message(STATUS "pybind11_FOUND: ${pybind11_FOUND}")
+message(STATUS "pybind11_VERSION: ${pybind11_VERSION}")
+message(STATUS "pybind11_INCLUDE_DIRS: ${pybind11_INCLUDE_DIRS}")
+
+if((NOT pybind11_FOUND) AND (NOT pybind11_POPULATED))
+  # If pybind11 is not found, fetch and add it
   FetchContent_Populate(pybind11)
   add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
 endif()
 
-# Execute python command to get pybind11 includes
-execute_process(
-    COMMAND python3 -m pybind11 --includes
-    OUTPUT_VARIABLE PYBIND11_INCLUDES
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-# Split the output into a list
-string(REPLACE " " ";" PYBIND11_INCLUDE_LIST ${PYBIND11_INCLUDES})
-
-# Remove the -I prefix from each path
-list(TRANSFORM PYBIND11_INCLUDE_LIST REPLACE "^-I" "")
-
-# Include the directories
-foreach(INCLUDE_DIR ${PYBIND11_INCLUDE_LIST})
-    include_directories(${INCLUDE_DIR})
-endforeach()
+# If pybind11 was fetched, print its source and binary directories
+message(STATUS "pybind11_SOURCE_DIR: ${pybind11_SOURCE_DIR}")
+message(STATUS "pybind11_BINARY_DIR: ${pybind11_BINARY_DIR}")
